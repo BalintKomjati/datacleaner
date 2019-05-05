140be3712c310ff517a68cd77289d75920da8ef7
Balint Komjati <bkomjati@gmail.com>
2019-05-05 09:48:01 +0200

    fine tuning in widsorize function

--- R/windsorize.R
+++ R/windsorize.R
@@ -1,10 +1,14 @@
-#' Windsorize
+#' Winsorize
 #'
-#' Do some windsorization.
+#' Well it does Win-motherfucking-sorization
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  if( all(is.na(x)) ) stop("Input must not contain only NAs!")
+  if( all(is.null(x)) ) stop("Input must contain data!")
+  qhigh <- quantile(x, 1-(1-p)/2, na.rm=TRUE)
+  qlow <- quantile(x, (1-p)/2, na.rm=TRUE)
+  x[x >= qhigh] <- qhigh
+  x[x <= qlow] <- qlow
   x
 }
 
